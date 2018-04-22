c19eda3c51347ea83514b6c2c68786c9fc98e600
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2018-04-22 11:10:08 +0100

    Added utiltiy to make a zip file

---
+++ utilities/makezip.py
@@ -0,0 +1,22 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import zipfile
+
+if __name__ == "__main__":
+    cwd = os.getcwd()
+    rawpath = os.path.join(cwd, 'reprojected')
+    allfiles= [f for f in listdir(rawpath) if isfile(join(rawpath, f))]
+    af = [f.split('.')[0] for f in listdir(rawpath) if isfile(join(rawpath, f))]
+    onlyfiles =  (list(set(af)))
+
+    print(onlyfiles)
+    opfolder = os.path.join(cwd, 'output')
+    if not os.path.exists(opfolder):
+        os.mkdir(opfolder)
+    for file in onlyfiles: 
+        zfilename = os.path.join(opfolder,file+'.zip' )
+        with zipfile.ZipFile(zfilename, 'w') as myzip:
+            lstFileNames = [f1 for f1 in allfiles if f1.split('.')[0] == file]
+            for f in lstFileNames:   
+                myzip.write(os.path.join(rawpath,f), f)
